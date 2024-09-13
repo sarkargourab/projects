d4defafa09901ebf5b18f267124c04fca30dd174
GitGenX <gsarkar1309.@gmail.com>
2024-09-13 07:29:01 +0530

    This is the first commit

---
+++ sortingvisualizer/script.js
@@ -0,0 +1,59 @@
+const n = 10;
+const array = [];
+
+init();
+
+function init(){
+    for(let i= 0;i<n;i++){
+        array[i] = Math.random();
+    }
+    showBar();
+}
+
+function play() {
+    const copy = [...array];
+    const swaps = bubbleSort(copy);
+    animate(swaps);
+}
+
+function animate(swaps){
+    if (swaps.length == 0) {
+        showBar();
+        return;
+    }
+    const[i,j] = swaps.shift();
+    [array[i],array[j]] = [array[j],array[i]];
+    showBar([i,j]);
+    setTimeout (function(){
+        animate(swaps);
+    },50);
+}
+
+function bubbleSort(array){
+    const swaps = [];
+    do{
+        var swapped = false;
+        for(let i = 1;i<array.length;i++){
+            if(array[i-1]>array[i]){
+                swapped = true;
+                swaps.push([i-1,i]);
+                [array[i-1],array[i]]=[array[i],array[i-1]];
+            }
+        }
+    }while(swapped);
+    return swaps;
+}
+
+function showBar(indices){
+    container.innerHTML = "";
+    for(let i = 0; i<array.length;i++){
+        const bar = document.createElement("div");
+        bar.style.height = array[i]*100+"%";
+        bar.classList.add("bar");
+
+        if(indices && indices.includes(i)){
+            bar.style.backgroundColor = "red";
+        }
+        container.appendChild(bar);
+    }
+}
